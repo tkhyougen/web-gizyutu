d6a12473b7b4dd523f6f64991174256e41549f97
tomohiko <illgalsubstance@yahoo.co.jp>
2019-10-05 11:35:47 +0000

    web

---
+++ test.rb
@@ -0,0 +1,27 @@
+require 'webrick'
+
+module WEBrick
+  module HTTPServlet
+    FileHandler.add_handler('rb', CGIHandler)
+  end
+end
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+# WEBrick::HTTPServlet::FileHandlerをWEBrick::HTTPServlet::ERBHandlerに変更する
+# 'test.html'を'test.html.erb'に変更する
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/kadai',WEBrick::HTTPServlet::ERBHandler,'kadai.html.erb')
+
+# この一行を追記
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/goya2.cgi',WEBrick::HTTPServlet::CGIHandler, 'goya2.rb')
+
+server.start
